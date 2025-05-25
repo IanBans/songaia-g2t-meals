26ed46f218116d3f3e26deee8924aab53cbefbd1
Ian B <ibansenauer@gmail.com>
2025-05-25 16:53:37 -0700

    initial commit with firestore

---
+++ android/app/src/main/kotlin/club/gardentotable/g2t_meals/MainActivity.kt
@@ -0,0 +1,6 @@
+package club.gardentotable.g2t_meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class Colors {
+  static const Color black = Color(0xFF000000);
+  static const Color purple = Color(0xFF7b00ff);
+  static List<Color> primaryColors = const [
+     black,
+     purple
+  ];
+}
